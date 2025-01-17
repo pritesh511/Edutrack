e0af1311360dc481dc6b6ba8b541d6f239244673
Pritesh_Administer <apple_macbook_pro@Apple-MBPs-MBP.lan>
2025-01-17 10:48:59 +0530

    Design student table component

--- src/components/dashboard/pages/StudentPage.tsx
+++ src/components/dashboard/pages/StudentPage.tsx
@@ -1,13 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import AddStudentModal from "../dialogs/AddStudentModal";
@@ -15,12 +7,22 @@ import CustomTableHead from "@/components/common/CustomTableHead";
 import CustomTableRow from "@/components/common/CustomTableRow";
 import CustomTableCell from "@/components/common/CustomTableCell";
 import CustomTable from "@/components/common/CustomTable";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const StudentTabView = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const StudentTableHeader = () => {
-    const headers = ["Role No", "Name", "Class", "Attendance"];
+    const headers = [
+      "Role No",
+      "Name",
+      "Class",
+      "Attendance",
+      "Parent's Mobile",
+      "Address",
+      "Actions",
+    ];
     return (
       <>
         {headers.map((head) => (
@@ -37,6 +39,16 @@ const StudentTabView = () => {
         name: "Pritesh Makasana",
         class: "Standard 1",
         attendance: "78%",
+        parent_mo: "9081004687",
+        address: "A-303, Radhika recidency, surat",
+      },
+      {
+        role_no: 2,
+        name: "Pritesh Makasana",
+        class: "Standard 1",
+        attendance: "78%",
+        parent_mo: "9081004687",
+        address: "A-303, Radhika recidency, surat",
       },
     ];
     return (
@@ -53,10 +65,25 @@ const StudentTabView = () => {
           data.map((student, index) => {
             return (
               <CustomTableRow key={student.role_no}>
-                <CustomTableCell width={"5%"} cellName={index + 1} />
-                <CustomTableCell width={"15%"} cellName={student.name} />
-                <CustomTableCell width={"15%"} cellName={student.class} />
-                <CustomTableCell width={"15%"} cellName={student.attendance} />
+                <CustomTableCell width={"5%"} cellName={student.role_no} />
+                <CustomTableCell width={"10%"} cellName={student.name} />
+                <CustomTableCell width={"10%"} cellName={student.class} />
+                <CustomTableCell width={"10%"} cellName={student.attendance} />
+                <CustomTableCell width={"10%"} cellName={student.parent_mo} />
+                <CustomTableCell width={"10%"} cellName={student.address} />
+                <CustomTableCell
+                  width={"10%"}
+                  cellName={
+                    <div className="flex gap-3">
+                      <Button size="icon">
+                        <FaEdit />
+                      </Button>
+                      <Button size="icon" variant="destructive">
+                        <MdDelete />
+                      </Button>
+                    </div>
+                  }
+                />
               </CustomTableRow>
             );
           })
@@ -70,7 +97,7 @@ const StudentTabView = () => {
       <Card>
         <CardContent className="p-6">
           <div className="flex justify-between items-center mb-6">
-            <h2 className="text-xl font-semibold">Students List</h2>
+            <h2 className="text-xl font-semibold">Students</h2>
             <Button onClick={() => setIsModalOpen(true)} size={"lg"}>
               Add Student
             </Button>
