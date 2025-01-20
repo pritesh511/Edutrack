dfea023bc31220388183a5436462651d367b35fd
Pritesh_Administer <apple_macbook_pro@Apple-MBPs-MBP.lan>
2025-01-20 12:35:14 +0530

    Make dynamic api url

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -3,12 +3,16 @@ import { NextRequest } from "next/server";
 
 export async function getDataFromToken(request: NextRequest) {
   try {
-    const token = request.cookies.get("token")?.value || "";
+    const token = request.cookies.get("token")?.value;
+
+    if (!token) {
+      throw new Error("Token is missing in the request");
+    }
 
     const currentUser: any = jwt.verify(token, process.env.TOKEN_SECRET!);
 
     return currentUser.id;
   } catch (error: any) {
-    throw new Error(error.message);
+    throw new Error(`JWT Verification failed: ${error.message}`);
   }
 }
