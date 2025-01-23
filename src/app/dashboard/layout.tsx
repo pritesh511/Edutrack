4fa0ffdf09cf13af2a1d224a2aaf1fb67db9c04f
pritesh511 <patelpritesh313@gmail.com>
2025-01-23 19:33:02 +0530

    Add suspense loader

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
 import MobileSidebarOverlay from "@/components/common/MobileSidebarOverlay";
 import Header from "@/components/dashboard/Header";
 import Sidebar from "@/components/dashboard/Sidebar";
-import React from "react";
+import React, { Suspense } from "react";
+import DashboardLoading from "./loading";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,9 +10,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Sidebar />
       <div className="flex-1 overflow-hidden md:ml-64">
         <Header />
-        <main className="p-6 overflow-auto h-[calc(100vh-90px)]">
-          {children}
-        </main>
+        <Suspense fallback={<DashboardLoading />}>
+          <main className="p-6 overflow-auto h-[calc(100vh-90px)]">
+            {children}
+          </main>
+        </Suspense>
       </div>
       <MobileSidebarOverlay />
     </div>
